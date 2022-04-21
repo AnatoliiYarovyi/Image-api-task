dd608ae813bcb00c33bc8bd50c9052a0a20f1b88
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-04-21 18:58:39 +0300

    Update handler.ts

--- src/functions/imageUpload/handler.ts
+++ src/functions/imageUpload/handler.ts
@@ -1,18 +1,50 @@
+import AWS from 'aws-sdk';
+
 import { middyfy } from '../../libs/lambda';
 import { Event } from './interface';
 
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
 const handler = async (event: Event) => {
-  const image = event.body;
+  try {
+    const s3 = new AWS.S3();
+    const image = event.body;
+    const decodedFile = Buffer.from(
+      image.replace(/^data:image\/\w+;base64,/, ''),
+      'base64',
+    );
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: `images/${new Date().toISOString()}.jpg`,
+      Body: decodedFile,
+      ContentType: 'image/jpg',
+    };
 
-  const body = {
-    status: 'success',
-    message: `Email ${event.requestContext.authorizer.claims.email} has been authorized`,
-  };
+    const uploadResult = await s3.upload(params).promise();
 
-  return {
-    statusCode: 200,
-    body,
-  };
+    const body = {
+      status: 'success',
+      message: `Email ${event.requestContext.authorizer.claims.email} has been authorized`,
+      uploadResult,
+      image,
+    };
+
+    return {
+      statusCode: 200,
+      body,
+    };
+  } catch (e) {
+    console.error(e);
+    const body = {
+      status: 'error',
+      message: e,
+    };
+
+    return {
+      statusCode: 400,
+      body,
+    };
+  }
 };
 
 export const imageUpload = middyfy(handler);

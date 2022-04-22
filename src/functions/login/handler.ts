271bf7ebac8c48e48105abb1e53370b7b379c14e
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-04-22 16:24:18 +0300

    Update handler.ts

--- src/functions/login/handler.ts
+++ src/functions/login/handler.ts
@@ -4,32 +4,47 @@ import { middyfy } from '../../libs/lambda';
 import { Event } from './interface';
 
 const handler = async (event: Event) => {
-  const cognito = new AWS.CognitoIdentityServiceProvider();
+  try {
+    const cognito = new AWS.CognitoIdentityServiceProvider();
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const { email, password } = event.body;
-  const { user_pool_id, client_id } = process.env;
+    const { email, password } = event.body;
+    const { user_pool_id, client_id } = process.env;
 
-  const params = {
-    AuthFlow: 'ADMIN_NO_SRP_AUTH',
-    UserPoolId: user_pool_id,
-    ClientId: client_id,
-    AuthParameters: {
-      USERNAME: email,
-      PASSWORD: password,
-    },
-  };
-  const response = await cognito.adminInitiateAuth(params).promise();
+    const params = {
+      AuthFlow: 'ADMIN_NO_SRP_AUTH',
+      UserPoolId: user_pool_id,
+      ClientId: client_id,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+      },
+    };
+    const response = await cognito.adminInitiateAuth(params).promise();
 
-  const body = {
-    status: 'success',
-    message: 'Login successful',
-    token: response.AuthenticationResult.IdToken,
-  };
+    const resaltDb = await dynamodb
+      .get({ TableName: 'ImageTable', Key: { id: email } })
+      .promise();
 
-  return {
-    statusCode: 200,
-    body,
-  };
+    console.log('resaltDb: ', resaltDb);
+
+    const body = {
+      status: 'success',
+      message: 'Login successful',
+      token: response.AuthenticationResult.IdToken,
+      resaltDb,
+    };
+
+    return {
+      statusCode: 200,
+      body,
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: error,
+    };
+  }
 };
 
 export const login = middyfy(handler);

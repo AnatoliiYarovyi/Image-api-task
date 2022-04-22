d0ea05d3addafe7b5e181cc2a8a3b4b3115850a4
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-04-22 09:28:04 +0300

    Update handler.ts

--- src/functions/imageUpload/handler.ts
+++ src/functions/imageUpload/handler.ts
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import { v4 } from 'uuid';
 
 import { middyfy } from '../../libs/lambda';
 import { Event } from './interface';
@@ -8,6 +9,8 @@ const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 const handler = async (event: Event) => {
   try {
     const s3 = new AWS.S3();
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
     const { image } = event.body;
     const decodedFile = Buffer.from(
       image.replace(/^data:image\/\w+;base64,/, ''),
@@ -19,30 +22,35 @@ const handler = async (event: Event) => {
       Body: decodedFile,
       ContentType: 'image/jpeg',
     };
-
     const uploadResult = await s3.upload(params).promise();
+
+    const newImage = {
+      id: v4(),
+      email: event.requestContext.authorizer.claims.email,
+      imageLink: uploadResult.Location,
+    };
+    const putResult = await dynamodb
+      .put({ TableName: 'ImageTable', Item: newImage })
+      .promise();
 
     const body = {
       status: 'success',
       message: `Email ${event.requestContext.authorizer.claims.email} has been authorized`,
       uploadResult,
-      image,
+      putResult,
     };
 
     return {
       statusCode: 200,
       body,
     };
-  } catch (e) {
-    console.error(e);
-    const body = {
-      status: 'error',
-      message: e,
-    };
-
+  } catch (error) {
     return {
       statusCode: 400,
-      body,
+      body: {
+        status: 'error',
+        message: error,
+      },
     };
   }
 };

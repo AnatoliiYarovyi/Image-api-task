0895ecf6b33525869a5521853347a5f9dac9b527
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-04-28 18:35:45 +0300

    adds "joi" validation

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,11 +1,33 @@
 import middy from '@middy/core';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import Joi from 'joi';
 
 const middlewareEditResponse = (): middy.MiddlewareObj<
   APIGatewayProxyEvent,
   APIGatewayProxyResult
 > => {
+  const before: middy.MiddlewareFn = async (request): Promise<void> => {
+    const { email, password } = request.event.body;
+    const schema = Joi.object({
+      email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ['com', 'net'] },
+      }),
+      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    });
+    try {
+      const value = await schema.validateAsync({
+        email: email,
+        password: password,
+      });
+      console.log('value: ', value);
+      return;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
   const after: middy.MiddlewareFn<
     APIGatewayProxyEvent,
     APIGatewayProxyResult
@@ -21,7 +43,7 @@ const middlewareEditResponse = (): middy.MiddlewareObj<
     };
   };
 
-  return { after };
+  return { before, after };
 };
 
 export const middyfy = handler => {

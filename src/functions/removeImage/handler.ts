02b0b2e3562428aa22ea97a117c2d95663d7eb0f
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-04-29 12:00:22 +0300

    Update handler.ts

--- src/functions/removeImage/handler.ts
+++ src/functions/removeImage/handler.ts
@@ -3,17 +3,33 @@ import AWS from 'aws-sdk';
 import { middyfy } from '../../libs/lambda';
 import { Event } from './interface';
 
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
 const handler = async (event: Event) => {
   try {
+    const s3 = new AWS.S3();
     const dynamodb = new AWS.DynamoDB.DocumentClient();
     const email = event.requestContext.authorizer.claims.email;
     const { image } = event.pathParameters;
     const imageLink = `https://s3.amazonaws.com/image.s3.bucket/images/${image}`;
 
+    // remove image(object) from bucket
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: `images/${image}`,
+    };
+    s3.deleteObject(params, function (err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+      } else {
+        console.log(data);
+      }
+    });
+
+    // remove linkImage(object) from dynamoDB
     const resaltDb = await dynamodb
       .get({ TableName: 'ImageTable', Key: { id: email } })
       .promise();
-
     if (
       resaltDb.Item === undefined ||
       resaltDb.Item.imageLink.includes(imageLink) === false

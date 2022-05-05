1f17a39f054501b61e4ffeff8b07976c994ff13d
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-05-05 16:05:33 +0300

    Update handler.ts

--- src/functions/getAllImages/handler.ts
+++ src/functions/getAllImages/handler.ts
@@ -8,14 +8,41 @@ const handler = async (event: Event) => {
     const dynamodb = new AWS.DynamoDB.DocumentClient();
     const email = event.requestContext.authorizer.claims.email;
 
-    const resaltDb = await dynamodb
-      .get({ TableName: 'ImageTable', Key: { id: email } })
+    const currentUserDb = await dynamodb
+      .get({ TableName: 'Users', Key: { email: email } })
+      .promise();
+    const queryScan = {
+      TableName: 'Images',
+      FilterExpression: 'idUser = :this_idUser',
+      ExpressionAttributeValues: { ':this_idUser': currentUserDb.Item.id },
+    };
+    const images = {
+      imagesLink: {},
+    };
+    await dynamodb
+      .scan(queryScan, function (err, data) {
+        if (err) {
+          console.error(err);
+        } else {
+          const { Items } = data;
+          const imagesArr = Items.reduce((acc, el) => {
+            acc.push({
+              idImage: el.id,
+              image: el.imageLink,
+            });
+            return acc;
+          }, []);
+          console.log('imagesArr: ', imagesArr);
+
+          return (images.imagesLink = imagesArr);
+        }
+      })
       .promise();
 
     const body = {
       status: 'success',
       message: 'Get all images successful',
-      resaltDb,
+      imagesLink: images.imagesLink,
     };
 
     return {

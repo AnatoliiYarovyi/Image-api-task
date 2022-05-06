fe8dab02c7a348defbb2841c1b24dee34fce8132
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-05-06 11:03:43 +0300

    Update joiSchema.ts

--- src/libs/joiSchema.ts
+++ src/libs/joiSchema.ts
@@ -4,31 +4,15 @@ import { Request } from '../functions/interface/interface';
 
 const joiSchema = async (request: Request<string>) => {
   const path = request.event.rawPath || request.event.resource;
+  console.log('path: ', path);
 
   switch (path) {
-    case '/signup' || '/login':
-      const { email, password } = request.event.body;
-      const schema = Joi.object({
-        email: Joi.string()
-          .email({
-            minDomainSegments: 2,
-            tlds: { allow: ['com', 'net'] },
-          })
-          .required(),
-        password: Joi.string()
-          .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-          .required(),
-      });
-      try {
-        const value = await schema.validateAsync({
-          email: email,
-          password: password,
-        });
-        console.log('value: ', value);
-        return;
-      } catch (error) {
-        console.error(error);
-      }
+    case '/signup':
+      validateEmailPassword();
+      break;
+
+    case '/login':
+      validateEmailPassword();
       break;
 
     case '/getPresignedUrl':
@@ -42,6 +26,31 @@ const joiSchema = async (request: Request<string>) => {
 
     default:
       break;
+  }
+
+  async function validateEmailPassword() {
+    try {
+      const { email, password } = request.event.body;
+      const schema = Joi.object({
+        email: Joi.string()
+          .email({
+            minDomainSegments: 2,
+            tlds: { allow: ['com', 'net'] },
+          })
+          .required(),
+        password: Joi.string()
+          .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+          .required(),
+      });
+      const value = await schema.validateAsync({
+        email: email,
+        password: password,
+      });
+      console.log('value: ', value);
+    } catch (error) {
+      console.log(error);
+      throw new Error(`Error joi validate ${error}`);
+    }
   }
 };
 

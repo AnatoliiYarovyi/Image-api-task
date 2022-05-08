b95c29b2a2407bae47853444541264c5d77ecc8c
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-05-08 21:13:44 +0300

    Update lambda.ts

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,15 +1,56 @@
 import middy from '@middy/core';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
-import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-import joiSchema from './joiSchema';
-
-const middlewareJoiValidate = (): middy.MiddlewareObj<
+import type {
   APIGatewayProxyEvent,
-  APIGatewayProxyResult
-> => {
+  APIGatewayProxyResult,
+  Callback,
+  Context,
+} from 'aws-lambda';
+import { ObjectSchema } from 'joi';
+
+import { Event } from '../interface/interface';
+
+const middlewareJoiValidate = (
+  validateSchema: ObjectSchema,
+): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
   const before: middy.MiddlewareFn = async (request): Promise<void> => {
-    joiSchema(request);
+    const path = request.event.rawPath || request.event.resource;
+    try {
+      if (path === '/signup' || path === '/login') {
+        const { email, password } = request.event.body;
+        const value = await validateSchema.validateAsync({
+          email,
+          password,
+        });
+        console.log('value: ', value);
+      }
+      // if (path === '/getPresignedUrl' || path === '/getAllImages') {
+      //   const { Authorization } = request.event.headers;
+      //   const value = await validateSchema.validateAsync({
+      //     accessToken: Authorization,
+      //   });
+      //   console.log('value: ', value);
+      // }
+      // if (path === '/removeImage/{image}') {
+      //   const { Authorization } = request.event.headers;
+      //   const { image } = request.event.pathParameters;
+      //   const value = await validateSchema.validateAsync({
+      //     accessToken: Authorization,
+      //     imageName: image,
+      //   });
+      //   console.log('value: ', value);
+      // }
+    } catch (error) {
+      console.log(error);
+      // Initialize response
+      request.response = request.response ?? {};
+      // Add (.error) to response
+      request.response.error = `${error}`;
+      // Override an error
+      request.error = new Error(`Error joi validate ${error}`);
+      // handle the error
+      return request.response;
+    }
   };
   return { before };
 };
@@ -34,9 +75,15 @@ const middlewareEditResponse = (): middy.MiddlewareObj<
   return { after };
 };
 
-export const middyfy = handler => {
+export const middyfy = (
+  handler: {
+    (event: Event<any>);
+    (event: any, context: Context, callback: Callback<any>);
+  },
+  validateSchema: ObjectSchema,
+) => {
   return middy(handler)
     .use(middyJsonBodyParser())
-    .use(middlewareJoiValidate())
+    .use(middlewareJoiValidate(validateSchema))
     .use(middlewareEditResponse());
 };

e0f2a65c20f44599a5b9ca2f48550e1b5503ba3b
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-05-19 12:08:01 +0300

    Update handler.ts

--- src/functions/PreSignUp/handler.ts
+++ src/functions/PreSignUp/handler.ts
@@ -9,6 +9,7 @@ exports.handler = async (event: any, context: Context, callback: Callback) => {
     event.response.autoVerifyEmail = true;
   }
 
+  console.log('*** event ***: ', event);
   // Return to Amazon Cognito
   callback(null, event);
 };
